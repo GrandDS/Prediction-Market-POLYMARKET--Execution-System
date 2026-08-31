1005b26ab9a093c2ebeb58a681623590253c9aae
Tomi Kolawole <148702700+GrandDS@users.noreply.github.com>
2026-08-31 09:45:22 +0100

    Remove legacy strategy.py

--- strategy.py
+++
@@ -1,7 +0,0 @@
-class MarketStrategy:
-    def generate_signal(self, probability):
-        if probability > 0.70:
-            return "SELL"
-        if probability < 0.30:
-            return "BUY"
-        return "HOLD"
